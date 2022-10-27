e8e9949248b9b47539a88947a3c816b9cd64d8a6
TylerNguyen2 <106999071+TylerNguyen2@users.noreply.github.com>
2022-10-27 14:08:56 -0500

    Add files via upload

---
+++ cs201l-ss22-lab10-TylerNguyen2-main/Lab10/Fraction.h
@@ -0,0 +1,33 @@
+#pragma once
+// Your Name
+// Today's date
+// Lab 9
+
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+
+	// Add the variables and functions you need to. getGCD and reduce are 
+	// provided for you.
+
+private:
+	int denominator, numerator;
+	int getGCD(int num1, int num2);
+
+public:
+	
+    void reduce();
+    Fraction();
+    const Fraction operator+(Fraction rhs);
+    const Fraction operator-(Fraction rhs);
+    const Fraction operator*(Fraction rhs);
+    const Fraction operator/(Fraction rhs);
+    bool operator==(Fraction rhs);
+    friend ostream& operator<<(ostream& output, const Fraction& frac);
+    friend istream& operator>>(istream& input, Fraction& frac);
+
+
+};
+
---
+++ cs201l-ss22-lab10-TylerNguyen2-main/README.md
@@ -0,0 +1,8 @@
+# lab10-sp22
+
+# Student's name: Tyler Nguyen
+
+# Student's email address: thnhqh@umsystem.edu
+
+![image](https://user-images.githubusercontent.com/106999071/179135591-a46d8ab9-bf25-4ac6-8815-34026f2a5ac3.png)
+
